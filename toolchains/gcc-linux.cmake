8910227aa3dc9678d8452dc91637dd342d9843c2
Joseph Mellor <the.landfill.coding@gmail.com>
2020-05-25 06:38:21 -0500

    Switched to CMake

---
+++ toolchains/gcc-linux.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+add_compile_options(-std=c++11 -Wall -Wextra)
+add_compile_options(
+	"$<$<CONFIG:RELEASE>:-O3>"
+	"$<$<CONFIG:RELEASE>:-ffunction-sections>"
+	"$<$<CONFIG:RELEASE>:-fdata-sections>"
+	"$<$<CONFIG:RELEASE>:-flto>"
+	"$<$<CONFIG:DEBUG>:-O0>"
+	"$<$<CONFIG:DEBUG>:-g>"
+)
